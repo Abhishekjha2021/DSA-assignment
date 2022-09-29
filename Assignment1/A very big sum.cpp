73baadbd682bbbd481e9a04a509a68e2d1214b56
Abhishekjha2021 <114056135+Abhishekjha2021@users.noreply.github.com>
2022-09-29 10:41:37 -0600

    Create A very big sum.cpp

---
+++ Assignment1/A very big sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    unsigned long long int sum=0,in;
+    for(int i=0;i<n;i++)
+        {cin>>in;
+        sum+=in;}
+    cout<<sum;
+    return 0;
+}
